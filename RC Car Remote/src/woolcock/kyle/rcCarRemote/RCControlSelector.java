4e7df2d545a02392a975c2fc3dd0c3fc73206386
woolcock66 <kylewoolcock_10@hotmail.com>
2014-01-31 18:16:37 -0600

    Starting to set up Bluetooth / Layout

--- RC Car Remote/src/woolcock/kyle/rcCarRemote/RCControlSelector.java
+++ RC Car Remote/src/woolcock/kyle/rcCarRemote/RCControlSelector.java
@@ -1,10 +1,18 @@
 package woolcock.kyle.rcCarRemote;
 
-import android.os.Bundle;
+import java.util.Set;
+
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class RCControlSelector extends Activity {
+	
+	int REQUEST_ENABLE_BT = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +26,27 @@ public class RCControlSelector extends Activity {
 		getMenuInflater().inflate(R.menu.rccontrol_selector, menu);
 		return true;
 	}
+	
+	public void connectToDevice(View view) {
+		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+			//TODO Device does not support Bluetooth
+		}
+		if (!mBluetoothAdapter.isEnabled()) {
+		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		    //TODO progress bar for this activity
+		    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+		// If there are paired devices
+		if (pairedDevices.size() > 0) {
+		    // Loop through paired devices
+		    for (BluetoothDevice device : pairedDevices) {
+				// Add the name and address to an array adapter to show in a ListView
+		        mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+		    }
+		}
+	}
+	
 
 }
