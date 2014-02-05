e77c9d346a4e3c2f05f035b6f0f80f320650437d
woolcock66 <kylewoolcock_10@hotmail.com>
2014-02-05 12:59:48 -0600

    Started to add bluetooth support

--- RC Car Remote/src/woolcock/kyle/rcCarRemote/RCControlSelector.java
+++ RC Car Remote/src/woolcock/kyle/rcCarRemote/RCControlSelector.java
@@ -3,16 +3,23 @@ package woolcock.kyle.rcCarRemote;
 import java.util.Set;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 
 public class RCControlSelector extends Activity {
-	
+
 	int REQUEST_ENABLE_BT = 1;
+
+	ArrayAdapter<String> mArrayAdapter = new ArrayAdapter<String>(this,
+			android.R.layout.select_dialog_singlechoice);
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,27 +33,75 @@ public class RCControlSelector extends Activity {
 		getMenuInflater().inflate(R.menu.rccontrol_selector, menu);
 		return true;
 	}
-	
+
 	public void connectToDevice(View view) {
-		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		final BluetoothAdapter mBluetoothAdapter = BluetoothAdapter
+				.getDefaultAdapter();
 		if (mBluetoothAdapter == null) {
-			//TODO Device does not support Bluetooth
+			// Device does not support Bluetooth
+	        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	        builder.setMessage(R.string.bt_not_sup)
+	               .setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+	                   public void onClick(DialogInterface dialog, int id) {
+	                	   // Do nothing and halt the search
+	                       return;
+	                   }
+	               });
+	        builder.create().show();
 		}
 		if (!mBluetoothAdapter.isEnabled()) {
-		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-		    //TODO progress bar for this activity
-		    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+			// TODO progress bar for this activity
+			final ProgressDialog progDailog = ProgressDialog.show(this, "Progress_bar or give anything you want",
+		            "Please Wait....", true);
+		    new Thread() {
+		        public void run() {
+		            try {
+		    			Intent enableBtIntent = new Intent(
+		    					BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		            	startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		            	while(mBluetoothAdapter.getState() != BluetoothAdapter.STATE_ON) {
+		            		sleep(100);
+		            	}
+		            } catch (Exception e) {
+		            }
+		            progDailog.dismiss();
+		        }
+		    }.start();			
 		}
 		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
 		// If there are paired devices
 		if (pairedDevices.size() > 0) {
-		    // Loop through paired devices
-		    for (BluetoothDevice device : pairedDevices) {
-				// Add the name and address to an array adapter to show in a ListView
-		        mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-		    }
+			// Loop through paired devices
+			for (BluetoothDevice device : pairedDevices) {
+				// Add the name and address to an array adapter after getting current info
+				device = mBluetoothAdapter.getRemoteDevice(device.getAddress());
+				mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+			}
+		    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		    builder.setTitle(R.string.pick_device)
+		           .setItems(R.array.colors_array, new DialogInterface.OnClickListener() {
+		               public void onClick(DialogInterface dialog, int which) {
+		               // The 'which' argument contains the index position
+		               // of the selected item
+		           }
+		    }).setPositiveButton(R.string.search_for_device, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+             	   
+                }
+            });;
+		    builder.create().show();
 		}
 	}
-	
 
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_ENABLE_BT) {
+			if (resultCode == RESULT_OK) {
+
+			} else if (resultCode == RESULT_CANCELED) {
+
+			} else {
+
+			}
+		}
+	}
 }
